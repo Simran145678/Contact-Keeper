6f7f1edc4b50b2c4e6925a195ab4d88f52b3a453
HarsimranKaur1997 <hkaur607@my.centennialcollege.ca>
2021-10-16 19:09:08 -0400

    filter method completed for filtering contacts

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,16 +1,23 @@
 import React, { Fragment, useContext } from "react";
 import ContactItem from "./ContactItem";
-
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import ContactContext from "../../context/contact/contactContext";
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
+  if (contacts.length === 0) {
+    return <h4>Please add a contact</h4>;
+  }
   return (
     <Fragment>
-      {contacts.map((contact) => (
-        <ContactItem key={contact.id} contact={contact} />
-      ))}
+      {filtered !== null
+        ? filtered.map((contact) => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))
+        : contacts.map((contact) => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))}
     </Fragment>
   );
 };
