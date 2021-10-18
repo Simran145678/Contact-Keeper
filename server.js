9a6f6bb74c9f3c1f484fafb585aee421b2d3d581
HarsimranKaur1997 <hkaur607@my.centennialcollege.ca>
2021-10-18 17:13:20 -0400

    final commit and prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express(); //initialising express as app
+const path = require("path");
 
 app.get("/", (req, res) =>
   res.json({ msg: `Welcome to the contact keeper API...` })
@@ -15,6 +16,17 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+//Serve static assets in production
+
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.eventNames.PORT || 5000;
 
